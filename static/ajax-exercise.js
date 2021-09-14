9da95424bc337de8f2e18b0bbdf94df6fab286e6
Kaitlyn <kaitlyn.a.goodman@gmail.com>
2021-09-14 03:32:54 +0000

    finished exercise

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -27,6 +27,14 @@ function showWeather(evt) {
 
 
   // TODO: request weather with that URL and show the forecast in #weather-info
+
+
+  fetch(`${url}?zipcode=${zipcode}`)
+    .then((response) => response.json())
+    .then((weatherData) => {
+      document.querySelector("#weather-info").innerText = weatherData.forecast
+    })
+
 }
 
 document
@@ -40,5 +48,30 @@ function orderMelons(evt) {
 
   // TODO: show the result message after your form
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+  const formInputs = {
+    qty: document.querySelector("#qty-field").value,
+    melon_type: document.querySelector("#melon-type-field").value,
+  };
+
+  fetch("/order-melons.json", {
+    method: "POST",
+    body: JSON.stringify(formInputs),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  .then((response) => response.json())
+  .then((responseJson) => {
+    
+    if (responseJson.code === 'ERROR'){
+      document.querySelector("div#order-status").classList.add("order-error")
+      document.querySelector(".order-error").innerHTML = (responseJson.msg);
+      
+    } else if (responseJson.code === 'OK'){
+      document.querySelector("div#order-status").classList.remove("order-error")
+      document.querySelector("#order-status").innerHTML = responseJson.msg;
+    }
+  })
 }
 document.querySelector("#order-form").addEventListener("submit", orderMelons);
